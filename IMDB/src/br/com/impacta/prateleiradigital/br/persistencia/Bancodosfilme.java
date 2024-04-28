62952ef4481f628644ca5468f3dc8f617ad8404c
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-27 22:02:21 -0300

    Update Bancodosfilme.java

--- IMDB/src/br/com/impacta/prateleiradigital/br/persistencia/Bancodosfilme.java
+++ IMDB/src/br/com/impacta/prateleiradigital/br/persistencia/Bancodosfilme.java
@@ -1,5 +1,7 @@
 package br.com.impacta.prateleiradigital.br.persistencia;
 
 public class Bancodosfilme {
-
+	 public void salvarFilme(Filme filme) {
+	        // Implemente aqui a lógica para salvar um filme no banco de dados
+	    }
 }
