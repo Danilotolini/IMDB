7ac6cf680d6ab8ef34475fc9e73cc09ce5288756
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-27 22:00:28 -0300

    Update Filme.java

--- IMDB/src/br/com/impacta/prateleiradigital/br/negocio/Filme.java
+++ IMDB/src/br/com/impacta/prateleiradigital/br/negocio/Filme.java
@@ -1,5 +1,37 @@
 package br.com.impacta.prateleiradigital.br.negocio;
 
 public class Filme {
-
+	  private String titulo;
+	    private int ano;
+	    private String diretor;
+		public String getTitulo() {
+			return titulo;
+		}
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+		public int getAno() {
+			return ano;
+		}
+		public void setAno(int ano) {
+			this.ano = ano;
+		}
+		public String getDiretor() {
+			return diretor;
+		}
+		public void setDiretor(String diretor) {
+			this.diretor = diretor;
+		}
+		public Filme(String titulo, int ano, String diretor) {
+			super();
+			this.titulo = titulo;
+			this.ano = ano;
+			this.diretor = diretor;
+		} 
+		
+		public Filme() {
+			super();
+		} 
+	    
+	    
 }
