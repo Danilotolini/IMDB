29276a50d577f9284d101731e81882fab5763a34
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-27 22:02:48 -0300

    Update Controladorfilme.java

--- IMDB/src/br/com/impacta/prateleiradigital/br/controle/Controladorfilme.java
+++ IMDB/src/br/com/impacta/prateleiradigital/br/controle/Controladorfilme.java
@@ -1,5 +1,7 @@
 package br.com.impacta.prateleiradigital.br.controle;
 
 public class Controladorfilme {
-
+	 public void adicionarFilme(Filme filme) {
+	        // Implemente aqui a lógica para adicionar um filme
+	    }
 }
