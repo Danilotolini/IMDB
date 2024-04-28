4a13cfcc81472bc285f450fa5317f1548ae988ac
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-27 22:03:13 -0300

    Update Menuprincipal.java

--- IMDB/src/br/com/impacta/prateleiradigital/br/apresentacao/Menuprincipal.java
+++ IMDB/src/br/com/impacta/prateleiradigital/br/apresentacao/Menuprincipal.java
@@ -1,5 +1,7 @@
 package br.com.impacta.prateleiradigital.br.apresentacao;
 
 public class Menuprincipal {
-
+	 public static void exibirMenu() {
+	        // Implemente aqui a lógica para exibir o menu principal da aplicação
+	    }
 }
