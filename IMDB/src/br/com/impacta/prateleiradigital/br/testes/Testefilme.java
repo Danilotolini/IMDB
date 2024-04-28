82b31e196445e7137d25eabe0ee154939093d4f9
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-27 22:01:52 -0300

    Update Testefilme.java

--- IMDB/src/br/com/impacta/prateleiradigital/br/testes/Testefilme.java
+++ IMDB/src/br/com/impacta/prateleiradigital/br/testes/Testefilme.java
@@ -1,5 +1,7 @@
 package br.com.impacta.prateleiradigital.br.testes;
 
 public class Testefilme {
-
+	 public static void main(String[] args) {
+	        // Implemente aqui os testes unitários para a classe Filme
+	    }
 }
