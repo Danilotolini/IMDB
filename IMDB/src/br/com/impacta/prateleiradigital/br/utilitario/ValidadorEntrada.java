fd34745ac562d4c456b21d19cb6e9f3adea3072d
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-27 22:01:18 -0300

    Update ValidadorEntrada.java

--- IMDB/src/br/com/impacta/prateleiradigital/br/utilitario/ValidadorEntrada.java
+++ IMDB/src/br/com/impacta/prateleiradigital/br/utilitario/ValidadorEntrada.java
@@ -1,5 +1,8 @@
 package br.com.impacta.prateleiradigital.br.utilitario;
 
 public class ValidadorEntrada {
-
+	 public static boolean validarNumeroInteiro(String entrada) {
+	        // Implemente aqui a lógica para validar um número inteiro
+	        return false;
+	    }
 }
